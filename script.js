a4a8be684c8fc18bc97d1048d1c3f490500c23f6
Anjay kumar <a95633981@gmail.com>
2024-08-06 23:50:16 +0530

    add_apis

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-// const menuBar = document.querySelector("#abc");
-// const sideBar = document.querySelector(".sideBar");
-// const container = document.querySelector(".container")
-// menuBar.onclick = () => {
-//     sideBar.classList.toggle("small_sidebar");
-//     container.classList.toggle("large_container");
-// }
 
 
-// const APIKey = AIzaSyBKtNXbJ-eHEGygXXMLQVQrqUn1I215u2E;
 
-//https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyBKtNXbJ-eHEGygXXMLQVQrqUn1I215u2E
-
-
-//(`${BASE_URL}/search?key=${API_KEY}&q=${searchQuery}&maxResults=${maxItems}&part=snippet`);
+var allData;
 
 fetch("https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyBKtNXbJ-eHEGygXXMLQVQrqUn1I215u2E&q=$apple&maxResults=10&part=snippet")
   .then((response) => response.json())  // Parse the JSON from the response
   .then((results) => {
-    console.log(results); 
+    console.log(results);
+    allData = results;
+    videoShow();
+
+    // console.log(allData.items[0].id.videoId);
   })
   .catch((error) => {
-    
-    console.error('Error:', error);  
+
+    console.error('Error:', error);
   });
+
+const videoShow = () => {
+  const videos_container = document.getElementById("videos_container");
+  allData.items.map((val, index) => {
+
+    console.log(val.id.videoId)
+    const newBox = document.createElement("iframe");
+    newBox.src = `https://www.youtube.com/embed/${val.id.videoId}?si=RYjWO2x3onpXtIfC`
+    videos_container.appendChild(newBox);
+
+    
+
+  })
+
+
+}
